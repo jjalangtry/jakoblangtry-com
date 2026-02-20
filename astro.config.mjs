fb58a8edd1c17eb6b55194a7fa23c3cc0b8a0168
Jakob Langtry <113549052+jjl4287@users.noreply.github.com>
2026-02-19 19:08:25 -0500

    udpate build to use production and allow jjalangtry.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  vite: {
+    server: {
+      allowedHosts: [
+        'jakoblangtry.com',
+        'www.jakoblangtry.com',
+        'jjalangtry.com',
+        'www.jjalangtry.com'
+      ]
+    },
+    preview: {
+      allowedHosts: [
+        'jakoblangtry.com',
+        'www.jakoblangtry.com',
+        'jjalangtry.com',
+        'www.jjalangtry.com'
+      ]
+    }
+  }
 });
